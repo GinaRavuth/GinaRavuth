ed8ed3ed5e82705528e854895ae25e002c64888e
Gina <ginaravuth@gmail.com>
2017-05-17 01:35:10 -0400

    add pages

--- js/script.js
+++ js/script.js
@@ -8,5 +8,53 @@ $(document).ready(function() {
         duration: 900
     });
 
+    $("#ordino").click(function() {
+        $("#port-content").show("slow");
+        $("#port-content").load("./html/_ordino.html");
+
+         $(".portfolio").hide("slow");
+
+        $(".about").css({
+            "display": "none"
+        });
+        $(".contact").css({
+            "display": "none"
+        });
+    });
+
+    $("#portfolio-link").click(function () {
+        $(".portfolio").show("slow");
+
+        $(".about").css({
+            "display": "none"
+        });
+        $(".contact").css({
+            "display": "none"
+        });
+    });
+
+    $("#contact-link").click(function () {
+        $(".contact").show("slow");
+        
+        $(".about").css({
+            "display": "none"
+        });
+        $(".portfolio").css({
+            "display": "none"
+        });
+    });
+
+    $("#about-link").click(function () {
+        $(".about").show("slow");
+
+        $(".contact").css({
+            "display": "none"
+        });
+        $(".portfolio").css({
+            "display": "none"
+        });
+                $("#port-content").hide("slow");
+    });
+
 });
 
